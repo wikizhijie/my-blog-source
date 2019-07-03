c2b096ce0aacc23630f58e5f5b977435f483ad54
wikiLj <1262322118@qq.com>
2019-07-03 18:31:08 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,298 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="responsive-nav">
+      <i class="fa fa-bars" id="menu-toggle"></i>
+      <div id="menu" class="menu">
+        <i class="fa fa-times" id="menu-close"></i>
+        <div class="container">
+          <div class="image">
+            <a href="#"
+              ><img src="./assets/images/author-image.jpeg" alt=""
+            /></a>
+          </div>
+          <div class="author-content">
+            <h4>wiki</h4>
+            <span>Frontend devloper</span>
+          </div>
+          <nav class="main-nav" role="navigation">
+            <ul class="main-menu">
+              <li
+                @click="handleTabClick('proface')"
+                :class="{ active: active === 'proface' }"
+              >
+                <a>前言</a>
+              </li>
+              <li
+                @click="handleTabClick('fp')"
+                :class="{ active: active === 'fp' }"
+              >
+                <a>函数式编程</a>
+              </li>
+              <li
+                @click="handleTabClick('vue')"
+                :class="{ active: active === 'vue' }"
+              >
+                <a>Vue</a>
+              </li>
+              <li
+                @click="handleTabClick('cm')"
+                :class="{ active: active === 'cm' }"
+              >
+                <a>Contact Me</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="social-network">
+            <ul class="soial-icons">
+              <li>
+                <a href="https://fb.com/templatemo"
+                  ><i class="fa fa-facebook"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-rss"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view />
+    <section class="section about-me" data-section="section1">
+      <router-view />
+    </section>
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      active: "proface"
+    };
+  },
+  methods: {
+    handleTabClick(tabtext) {
+      this.active = tabtext;
+    }
+  }
+};
+</script>
+<style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji", "Lato", sans-serif;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: left;
+  background-color: #fff;
 
-<style>
+  li {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+}
+</style>
+<style lang="scss" scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: url(./assets/images/page-bg.jpg) no-repeat center center fixed;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+
+  .menu {
+    left: 10%;
+    -webkit-transition: -webkit-transform 233ms cubic-bezier(0, 0, 0.21, 1);
+    -webkit-overflow-scrolling: touch;
+    background-color: #a43f49;
+    box-sizing: border-box;
+    height: 100vh;
+    max-height: 100vh !important;
+    max-width: 80vw !important;
+    min-width: 45px !important;
+    outline: none;
+    overflow-x: hidden !important;
+    overflow-y: auto !important;
+    padding: 0;
+    position: fixed !important;
+    top: calc(90px);
+    width: 20%;
+    will-change: transform;
+    z-index: 9999 !important;
+    text-align: center;
+
+    @media screen and (max-width: 1680px) {
+      left: 5%;
+    }
+
+    .container {
+      margin: 0 auto;
+
+      .image {
+        margin-top: 60px;
+
+        a {
+          text-decoration: none;
+
+          img {
+            width: 140px;
+            height: 140px;
+            border-radius: 50%;
+            text-align: center;
+          }
+        }
+      }
+    }
+    .author-content {
+      margin: 40px 0px 60px 0px;
+      h4 {
+        font-size: 26px;
+        font-weight: 700;
+        color: #fff;
+        letter-spacing: 0.25px;
+      }
+      span {
+        font-size: 13px;
+        font-style: italic;
+        color: #fff;
+      }
+    }
+    .main-menu {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+
+      li {
+        &:hover {
+          a {
+            background: #fff;
+            color: #8c363e;
+          }
+        }
+        &.active {
+          a {
+            background: #fff;
+            color: #8c363e;
+          }
+        }
+        a {
+          height: 70px;
+          line-height: 70px;
+          display: inline-block;
+          font-size: 16px;
+          font-weight: 700;
+          color: #fff;
+          width: 100%;
+          transition: all 0.5s;
+          border-top: 2px solid #fff;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+  .section {
+    color: #fff;
+    width: 55%;
+    right: 10%;
+    left: 35%;
+    position: relative;
+    min-height: 100vh;
+    padding-bottom: 80px;
+    border-bottom: 3px solid rgba(250, 250, 250, 0.25);
+
+    @media screen and (max-width: 1680px) {
+      width: 65%;
+      right: 5%;
+      left: 30%;
+    }
+    .container {
+      width: 100%;
+      padding-right: 15px;
+      padding-left: 15px;
+      text-align: left;
+
+      @media (min-width: 576px) {
+        max-width: 540px;
+      }
+      @media (min-width: 768px) {
+        max-width: 720px;
+      }
+      @media (min-width: 992px) {
+        max-width: 960px;
+      }
+      @media (min-width: 1200px) {
+        max-width: 1140px;
+      }
+      .section-heading {
+        text-align: center;
+        padding: 80px 45px;
+
+        h2 {
+          font-size: 32px;
+          font-weight: 700;
+          color: #fff;
+        }
+        .line-dec {
+          width: 60px;
+          height: 3px;
+          background-color: #a43f49;
+          margin: 10px auto;
+        }
+        span {
+          margin-top: 5px;
+          font-size: 16px;
+          color: #fff;
+          letter-spacing: 0.5px;
+        }
+      }
+      .row {
+        display: flex;
+        flex-wrap: wrap;
+        margin-right: -15px;
+        margin-left: -15px;
+
+        .col-md-6 {
+          position: relative;
+          width: 100%;
+          padding-right: 15px;
+          padding-left: 15px;
+
+          @media (min-width: 768px) {
+            flex: 0 0 50%;
+            max-width: 50%;
+          }
+        }
+      }
+      .right-image-post {
+        margin-top: 60px;
+        padding-top: 60px;
+        border-top: 2px solid rgba(250, 250, 250, 0.1);
+      }
+    }
+  }
 }
+
 #nav {
   padding: 30px;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,131 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="section-heading">
+        <h2>wiki的博客</h2>
+        <div class="line-dec"></div>
+        <span
+          >想想做前端也三年了，一直想着弄一个自己的博客，也就是现在才抽出空来自己写了一个，也正好想用vue做这么一个博客，后期应该会加上用node做的后台，和管理后台，自己在这里写一些自己的想法跟对自己的知识归纳吧,
+          额..... 好像还有点简历的感觉</span
+        >
+      </div>
+      <div class="left-image-post">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="left-image">
+              <img src="assets/images/func.jpeg" alt="" />
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-text">
+              <h4>JavaScript 函数式编程 初尝试</h4>
+              <p>
+                最近在看关于JavaScript函数式编程，恰巧也是因为react跟vue3.0推动，让更多前端开发的同学认知到函数式编程带来的一些优势，我也写一些最近自己一些刚刚开始学习函数式编程道路上的心得及感悟吧.....
+              </p>
+              <div class="white-button">
+                <a href="#">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="right-image-post">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="left-text">
+              <h4>关于页面权限的一些想法</h4>
+              <p>
+                在做一些管理后台的网站的时候，很多人都会有这样的需求，就是关于权限的问题....
+              </p>
+              <div class="white-button">
+                <a href="#">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/right-image.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  .container {
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    text-align: left;
+
+    @media (min-width: 576px) {
+      max-width: 540px;
+    }
+    @media (min-width: 768px) {
+      max-width: 720px;
+    }
+    @media (min-width: 992px) {
+      max-width: 960px;
+    }
+    @media (min-width: 1200px) {
+      max-width: 1140px;
+    }
+    .section-heading {
+      text-align: center;
+      padding: 80px 45px;
+
+      h2 {
+        font-size: 32px;
+        font-weight: 700;
+        color: #fff;
+      }
+      .line-dec {
+        width: 60px;
+        height: 3px;
+        background-color: #a43f49;
+        margin: 10px auto;
+      }
+      span {
+        margin-top: 5px;
+        font-size: 16px;
+        color: #fff;
+        letter-spacing: 0.5px;
+      }
+    }
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+      margin-right: -15px;
+      margin-left: -15px;
+
+      .col-md-6 {
+        position: relative;
+        width: 100%;
+        padding-right: 15px;
+        padding-left: 15px;
+
+        @media (min-width: 768px) {
+          flex: 0 0 50%;
+          max-width: 50%;
+        }
+      }
+    }
+    .right-image-post {
+      margin-top: 60px;
+      padding-top: 60px;
+      border-top: 2px solid rgba(250, 250, 250, 0.1);
+    }
+  }
+}
+</style>
